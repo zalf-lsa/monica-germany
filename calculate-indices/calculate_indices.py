4a5fc11df5baae6ba1293c3ba582f029d753a43a
TStella <tommaso.stella@zalf.de>
2018-01-31 08:57:02 +0100

    indices calculation

--- calculate-indices/calculate_indices.py
+++ calculate-indices/calculate_indices.py
@@ -4,18 +4,20 @@ from collections import defaultdict
 import csv
 import numpy as np
 
-basepath = os.path.dirname(os.path.abspath(__file__))
-out_dir = basepath + "/out/"
+config = {
+    "elevation-filter": -9999,
+    "gwaterlevel-filter": 9999,
+    "write_files_for_R": False,
+    "disregard": {
+        #solve problems with last sim year and winter crops (duplicated files, one filled of 0 values)
+        "year": 2012,
+        "cm_count": 15
+    }
+}
 
-def walklevel(some_dir, level=1):
-    some_dir = some_dir.rstrip(os.path.sep)
-    assert os.path.isdir(some_dir)
-    num_sep = some_dir.count(os.path.sep)
-    for root, dirs, files in os.walk(some_dir):
-        yield root, dirs, files
-        num_sep_this = root.count(os.path.sep)
-        if num_sep + level <= num_sep_this:
-            del dirs[:]
+basepath = os.path.dirname(os.path.abspath(__file__))
+main_out_dir = "Z:/projects/monica-germany/calibration-sensitivity-runs/2018-01-30-full/"#basepath + "/out/"
+out_for_R_script = {}
 
 def representsInt(s):
     try: 
@@ -31,107 +33,6 @@ def representsfloat(s):
     except ValueError:
         return False
 
-#associate landkreise (lk) to pedoclimatic regions (bkr)
-lk_2_bkr = defaultdict(set)
-bkr_2_lk = defaultdict(set)
-sim_lk = set()
-
-with open("landkreise_bkrs.csv") as _:
-    reader = csv.reader(_)
-    next(reader, None)
-    for row in reader:
-        lk = int(row[0])
-        for i in range(1, len(row)):
-            if representsInt(row[i]):
-                bkr = int(row[i])
-                lk_2_bkr[lk].add(bkr)
-                bkr_2_lk[bkr].add(lk)
-                sim_lk.add(lk)
-
-#read lk elevation/latitude
-topography = defaultdict(lambda: defaultdict(float))
-with open("avg_elevation_latitude_per_landkreis.csv") as _:
-    reader = csv.reader(_)
-    next(reader, None)
-    for row in reader:
-        lk = int(row[0])
-        topography[lk]["lat"] = float(row[1])
-        topography[lk]["elev"] = float(row[2])
-
-#data[bkr/lk][year][sim/obs]
-yield_data = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
-maxLAI_data = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
-#all_years = set()
-all_years = range(1999, 2013) #both obs and sims available for these years!
-
-#read official yields
-obs_lk = set()
-obs_bkr = set()
-with open("official_yields_DE.csv") as _:
-    reader = csv.reader(_)
-    for i in range(7):
-        next(reader, None)
-    
-    #3= Winterweizen (WW)
-    #4=	Roggen und Wintermenggetreide (RY)
-    #5= Wintergerste (WB)
-    #6= Sommergerste (SB)
-    #7= Hafer (OA)
-    #8=	Triticale (TR)
-    #9=	Kartoffeln (PO)
-    #10= Zuckerrüben (SBee)
-    #11= Winterraps	(WRa)
-    #12= Silomais (SM)
-
-    for row in reader:
-        if len(row)>2 and representsInt(row[1]):
-            lk = int(row[1])
-            if lk in sim_lk:
-                year = int(row[0])
-                #all_years.add(year)
-                if representsfloat(row[3]):
-                    if float(row[3]) == 0.0:
-                        #consider 0 as nodata
-                        continue
-                    obs_lk.add(lk)
-                    for bkr in lk_2_bkr[lk]:
-                        obs_bkr.add(bkr)
-                    obs_yield = float(row[3]) * 100 #kg ha-1
-                    yield_data[lk][year]["obs"] = obs_yield
-
-#print len(obs_lk)
-
-#read output
-for root, dirs, filenames in walklevel(out_dir, level=1):
-    for filename in filenames:
-        year = int(filename.split("_")[2])
-        with open(out_dir + filename) as _:
-            reader = csv.reader(_)
-            next(reader, None)
-            for row in reader:
-                lk = int(row[0])
-                sim_var = float(row[1])
-                if "yield" in filename:
-                    yield_data[lk][year]["sim"] = sim_var
-                elif "max-LAI" in filename:
-                    maxLAI_data[lk][year]["sim"] = sim_var
-
-#aggregate to bkr, TODO weighted average
-for bkr, lks in bkr_2_lk.iteritems():
-    sims = []
-    obs = []
-    for year in all_years:
-        for lk in lks:
-            if "obs" in yield_data[lk][year].keys():
-                sims.append(yield_data[lk][year]["sim"])
-                obs.append(yield_data[lk][year]["obs"])
-        avg_sim = float(np.average(sims))
-        avg_obs = float(np.average(obs))
-        yield_data[bkr][year]["sim"] = avg_sim
-        yield_data[bkr][year]["obs"] = avg_obs
-
-
-#calculate indices
 def calculate_indices(obs, sim):
     out = defaultdict(float)
     out["RRMSE"] = spotpy.objectivefunctions.rrmse(obs, sim)
@@ -145,7 +46,7 @@ def calculate_indices(obs, sim):
     out["avg_sim"] = float(np.average(sim))
     return out
 
-def retrieve_data(ids, years, data=yield_data, obs_sim=""):
+def retrieve_data(ids, years, data, obs_sim):
     'returns data from selected bkrs/lks (i.e., ids) and years. obs_sim must be either "obs" or "sim"'
     out = []
     for id in ids:
@@ -160,7 +61,7 @@ def retrieve_data(ids, years, data=yield_data, obs_sim=""):
             #print(data[id][yr][obs_sim], id, yr, obs_sim)
     return out
 
-def find_obs_years(id, data=yield_data, yr_range=all_years):
+def find_obs_years(id, data, yr_range):
     'returns a list of years for which observed yields are available in the lk or bkr'
     out = []
     for yr in data[id].keys():
@@ -168,36 +69,186 @@ def find_obs_years(id, data=yield_data, yr_range=all_years):
             out.append(yr)
     return out
 
-indices_lks = defaultdict(lambda: defaultdict(float)) #[index][lk] = value for all years
-other_var_lks =  defaultdict(lambda: defaultdict(float)) #[var][lk] = value for all years
-for lk in obs_lk:
-    yrs = find_obs_years(lk)
-    obs = retrieve_data(ids=[lk], years=yrs, obs_sim="obs")
-    sim = retrieve_data(ids=[lk], years=yrs, obs_sim="sim")
-    indices = calculate_indices(obs, sim)
-    for id in indices.keys():
-        indices_lks[id][lk] = indices[id]
-    sim_maxLAI = retrieve_data(ids=[lk], years=yrs, data=maxLAI_data, obs_sim="sim")
-    other_var_lks["max-LAI"][lk] = float(np.average(sim_maxLAI))
+def check_obs_sim_length(obs, sim, entity):
+    if len(obs) != len(sim):
+        print("!!obs and sim lists of " + entity + " have different length!!")
+
+exp_dirs = [x[1] for x in os.walk(main_out_dir)][0]
+for exp_folder in exp_dirs:
+    calib_dirs = [x[1] for x in os.walk(main_out_dir + exp_folder + "/")][0]
+    for calib_folder in calib_dirs:
+        out_dir = main_out_dir + exp_folder + "/" + calib_folder + "/"
+        print("starting the analysis of " + out_dir)
+
+        #associate landkreise (lk) to pedoclimatic regions (bkr)
+        lk_2_bkr = defaultdict(set)
+        bkr_2_lk = defaultdict(set)
+        sim_lk = set()
+
+        with open("landkreise_bkrs.csv") as _:
+            reader = csv.reader(_)
+            next(reader, None)
+            for row in reader:
+                lk = int(row[0])
+                for i in range(1, len(row)):
+                    if representsInt(row[i]):
+                        bkr = int(row[i])
+                        lk_2_bkr[lk].add(bkr)
+                        bkr_2_lk[bkr].add(lk)
+                        sim_lk.add(lk)
+
+        #read lk elevation/latitude
+        topography = defaultdict(lambda: defaultdict(float))
+        with open("avg_elevation_latitude_gw_per_landkreis.csv") as _:
+            reader = csv.reader(_)
+            next(reader, None)
+            for row in reader:
+                lk = int(row[0])
+                topography[lk]["lat"] = float(row[1])
+                topography[lk]["elev"] = float(row[2])
+                topography[lk]["gwl"] = float(row[2])
+
+        #data[bkr/lk][year][sim/obs]
+        yield_data = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
+        maxLAI_data = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
+        #all_years = set()
+        all_years = range(1999, 2013) #both obs and sims available for these years!
+
+        #read official yields
+        obs_lk = set()
+        obs_bkr = set()
+        with open("official_yields_DE.csv") as _:
+            reader = csv.reader(_)
+            for i in range(7):
+                next(reader, None)
+            
+            #3= Winterweizen (WW)
+            #4=	Roggen und Wintermenggetreide (RY)
+            #5= Wintergerste (WB)
+            #6= Sommergerste (SB)
+            #7= Hafer (OA)
+            #8=	Triticale (TR)
+            #9=	Kartoffeln (PO)
+            #10= Zuckerruben (SBee)
+            #11= Winterraps	(WRa)
+            #12= Silomais (SM)
+
+            for row in reader:
+                if len(row)>2 and representsInt(row[1]):
+                    lk = int(row[1])
+                    #filter lks for elevation/groundwater leve
+                    if topography[lk]["elev"] < config["elevation-filter"]:
+                        continue
+                    if topography[lk]["gwl"] > config["gwaterlevel-filter"]:
+                        continue
+                    if lk in sim_lk:
+                        year = int(row[0])
+                        #all_years.add(year)
+                        if representsfloat(row[3]):
+                            if float(row[3]) == 0.0:
+                                #consider 0 as nodata
+                                continue
+                            obs_lk.add(lk)
+                            for bkr in lk_2_bkr[lk]:
+                                obs_bkr.add(bkr)
+                            obs_yield = float(row[3]) * 100 #kg ha-1
+                            yield_data[lk][year]["obs"] = obs_yield
+
+        #print len(obs_lk)
+
+        #read output
+        #exp_files = [x[2] for x in os.walk(out_dir + "no_calibration/aggregated/")][0]
+        exp_files = [x[2] for x in os.walk(out_dir + "aggregated/")][0]
+        for filename in exp_files:
+            year = int(filename.split("_")[2])
+            cm_count = int(filename.split("_")[3])
+            if ".csv" not in filename:
+                continue #skip grids
+            if year == config["disregard"]["year"] and cm_count == config["disregard"]["cm_count"]:
+                continue #skip duplicated out files
+            with open(out_dir + "aggregated/" + filename) as _:
+                reader = csv.reader(_)
+                next(reader, None)
+                for row in reader:
+                    lk = int(row[0])
+                    sim_var = float(row[1])
+                    if "yield" in filename:
+                        yield_data[lk][year]["sim"] = sim_var
+                    elif "max-LAI" in filename:
+                        maxLAI_data[lk][year]["sim"] = sim_var
+                
+        #calculate indices
+        indices_all_lks_x_yrs = defaultdict(float)
+        all_obs = []
+        all_sim =[]
+        indices_lks = defaultdict(lambda: defaultdict(float)) #[index][lk] = value for all years
+        other_var_lks =  defaultdict(lambda: defaultdict(float)) #[var][lk] = value for all years
+
+        for lk in obs_lk:
+            yrs = find_obs_years(lk, yield_data, all_years)
+            obs = retrieve_data(ids=[lk], years=yrs, data=yield_data, obs_sim="obs")
+            sim = retrieve_data(ids=[lk], years=yrs, data=yield_data, obs_sim="sim")
+            check_obs_sim_length(obs, sim, "lk"+str(lk))
+            all_obs += obs
+            all_sim += sim
+            indices = calculate_indices(obs, sim)
+            for id in indices.keys():
+                indices_lks[id][lk] = indices[id]
+            sim_maxLAI = retrieve_data(ids=[lk], years=yrs, data=maxLAI_data, obs_sim="sim")
+            other_var_lks["max-LAI"][lk] = float(np.average(sim_maxLAI))
+
+        indices = calculate_indices(all_obs, all_sim)
+        for id in indices.keys():
+            indices_all_lks_x_yrs[id] = indices[id]
+        out_for_R_script[int(exp_folder)] = indices_all_lks_x_yrs
 
 
-#write report
-with open("report_indices.csv", "wb") as _:
-    writer = csv.writer(_)
-    indices_header = ["RRMSE", "pBIAS", "NSE", "AI", "r", "r2", "n_obs", "avg_obs", "avg_sim"]
-    
-    header = ["lk x all years"] + indices_header + ["latitude", "elevation", "max-LAI"]
-    writer.writerow(header)
-    for lk in obs_lk:
-        if indices_lks["n_obs"][lk] < 3:
-            continue
-        row=[lk]
-        for id in indices_header:
-            row.append(str(indices_lks[id][lk]))
-        row.append(topography[lk]["lat"])
-        row.append(topography[lk]["elev"])
-        row.append(str(other_var_lks["max-LAI"][lk]))
-        writer.writerow(row)
+        #write report
+        report_name = "report_indices_exp" + exp_folder + "_calib" + calib_folder + ".csv" 
+        with open("report/" + report_name, "wb") as _:
+            writer = csv.writer(_)
+            indices_header = ["RRMSE", "pBIAS", "NSE", "AI", "r", "r2", "n_obs", "avg_obs", "avg_sim"]
+            
+            #all lks
+            header = ["all lks x all years"] + indices_header
+            row= []
+            row.append("")
+            for id in indices_header:
+                row.append(str(indices_all_lks_x_yrs[id]))
+            writer.writerow(header)
+            writer.writerow(row)
+
+            #each lk
+            header = ["lk x all years"] + indices_header + ["latitude", "elevation", "max-LAI"]
+            writer.writerow(header)
+            for lk in obs_lk:
+                if indices_lks["n_obs"][lk] < 3:
+                    continue
+                row=[lk]
+                for id in indices_header:
+                    row.append(str(indices_lks[id][lk]))
+                row.append(topography[lk]["lat"])
+                row.append(topography[lk]["elev"])
+                row.append(str(other_var_lks["max-LAI"][lk]))
+                writer.writerow(row)
+        print(report_name + " written!")
+
+#produce csv for r script
+if config["write_files_for_R"]:
+    for exp in sorted(out_for_R_script.keys()):
+        with open("report/" + "RRMSE.csv", "ab") as _:
+            writer = csv.writer(_)
+            writer.writerow([out_for_R_script[exp]["RRMSE"]])
+        with open("report/" + "pBIAS.csv", "ab") as _:
+            writer = csv.writer(_)
+            writer.writerow([out_for_R_script[exp]["pBIAS"]])
+        with open("report/" + "AI.csv", "ab") as _:
+            writer = csv.writer(_)
+            writer.writerow([out_for_R_script[exp]["AI"]])
+        with open("report/" + "r.csv", "ab") as _:
+            writer = csv.writer(_)
+            writer.writerow([out_for_R_script[exp]["r"]])
+    print("files for R script written!")
 
 print("finished!")
 exit()
@@ -213,7 +264,19 @@ exit()
 
 
 
-
+#aggregate to bkr, TODO weighted average
+for bkr, lks in bkr_2_lk.iteritems():
+    sims = []
+    obs = []
+    for year in all_years:
+        for lk in lks:
+            if "obs" in yield_data[lk][year].keys():
+                sims.append(yield_data[lk][year]["sim"])
+                obs.append(yield_data[lk][year]["obs"])
+        avg_sim = float(np.average(sims))
+        avg_obs = float(np.average(obs))
+        yield_data[bkr][year]["sim"] = avg_sim
+        yield_data[bkr][year]["obs"] = avg_obs
 
 
 
--- reduced_factorial_design.R
+++ reduced_factorial_design.R
@@ -1,21 +1,95 @@
 library(FrF2)
-setwd("C:/Users/stella/Desktop/ZALF/Laura_Klement/evaluation")
-setwd("C:/Users/berg.ZALF-AD/GitHub/monica-germany")
+setwd("C:/Users/stella/GitHub/monica-germany")
+#setwd("C:/Users/berg.ZALF-AD/GitHub/monica-germany")
 
 design <- FrF2(resolution = 4, randomize = FALSE, factor.names = list(
   GroundWaterLevel = c("false", "true"), 
   ImpenetrableLayer = c("false", "true"), 
-  #Skeleton = c("false", "true"), 
+  Phenology = c("default", "calibrated"), #decide wether to 
   Slope = c("false", "true"), 
   SowingDate = c("fixed", "auto"),
   HarvestDate = c("fixed", "auto"),
   LandCover = c("false", "true"), 
   Nresponse_and_Fertil = c("false", "true"), 
-  WaterDeficitResponse = c("false", "true"), 
-  LeafExtensionModifier = c("false", "true"), 
-  #Calibration = c("false", "true")
+  WaterDeficitResponse = c("false", "true"),
+  FCM_Calibration = c("false", "true"),
+  LeafExtensionModifier = c("false", "true")
   ))
  
-design
+#design
 
-export.design(design, filename = "design", type = "csv", OutDec = ".") 
+summary(design)
+
+
+export.design(design, filename = "design_complete", type = "csv", OutDec = ".") 
+
+res_r <- read.csv("C:/Users/stella/Documents/GitHub/monica-germany/calculate-indices/report_250118/shallow_groundwater/r.csv",header=F)
+res_rrmse <- read.csv("C:/Users/stella/Documents/GitHub/monica-germany/calculate-indices/report_250118/shallow_groundwater/RRMSE.csv",header=F)
+res_pbias <- read.csv("C:/Users/stella/Documents/GitHub/monica-germany/calculate-indices/report_250118/shallow_groundwater/pBIAS.csv",header=F)
+res_ai <- read.csv("C:/Users/stella/Documents/GitHub/monica-germany/calculate-indices/report_250118/shallow_groundwater/AI.csv",header=F)
+
+resp_r <- add.response(design, res_r)
+resp_rrmse <- add.response(design, res_rrmse)
+resp_pbias <- add.response(design, res_pbias)
+resp_ai <- add.response(design, res_ai)
+
+plot(resp_rrmse, cex = 1, cex.lab = 0.8, cex.axis = 0.8,
+     main = "Main effects plot rrmse", cex.main = 2)
+
+IAPlot(resp_rrmse, abbrev = 5, show.alias = TRUE, lwd = 2, cex = 2,
+       cex.xax = 1.2, cex.lab = 1.5)
+
+DanielPlot(resp_rrmse, code = TRUE, half = TRUE, alpha = 0.1,
+           cex.main = 1.8, cex.pch = 1.2, cex.lab = 1.4, cex.fac = 1.4,
+           cex.axis = 1.2)
+
+
+#Definition of high elevation and shallow groundwater landkreise
+topography <- read.csv("C:/Users/stella/Documents/GitHub/monica-germany/avg_elevation_latitude_gw_per_landkreis.csv",header=T)
+elevations <- as.vector(topography['elevation'])
+gwater <- as.vector(topography['groundwaterlevel'])
+
+my_var = unlist(gwater)
+
+hist(my_var, breaks=5, col="red")
+
+x <- my_var 
+h<-hist(x, breaks=10, col="red",  
+        main="Histogram with Normal Curve") 
+xfit<-seq(min(x),max(x),length=40) 
+yfit<-dnorm(xfit,mean=mean(x),sd=sd(x)) 
+yfit <- yfit*diff(h$mids[1:2])*length(x) 
+lines(xfit, yfit, col="blue", lwd=2)
+
+#example of RFD below
+plan.annotated <- FrF2(16, 6, factor.names = list(
+  DieOrif = c(2.093, 2.1448), PistDiam = c(9.462, 9.5),
+  Temp = c(188.1, 191.1), DieClean = c("Dirty", "Clean"),
+  SMass = c(4, 8), BarClean = c("Dirty", "Clean")),
+  seed = 6285)
+
+summary(plan.annotated)
+
+MI <- c(35.77, 35.03, 38.5, 39.33, 35.7, 35.1, 39.27, 37, 41.07, 32.03,
+           42, 37.63, 40.2, 37, 40.1, 35.03)
+
+plan.resp <- add.response(plan.annotated, MI)
+
+plot(plan.resp, cex = 1.2, cex.lab = 1.2, cex.axis = 1.2,
+     main = "Main effects plot for MI", cex.main = 2)
+
+#MEPlot(plan.resp, abbrev = 5, cex.xax = 1.6, cex.main = 2)
+
+IAPlot(plan.resp, abbrev = 5, show.alias = TRUE, lwd = 2, cex = 2,
+       cex.xax = 1.2, cex.lab = 1.5)
+
+IAPlot(plan.resp, abbrev = 5, select = 3:6, lwd = 2, cex = 2,
+       cex.xax = 1.2, cex.lab = 1.5)
+
+summary(lm(plan.resp))
+
+DanielPlot(plan.resp, code = TRUE, half = TRUE, alpha = 0.1,
+           cex.main = 1.8, cex.pch = 1.2, cex.lab = 1.4, cex.fac = 1.4,
+           cex.axis = 1.2)
+
+summary(plan.resp)
